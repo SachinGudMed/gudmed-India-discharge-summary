3c7dda2c14b94d29fb1f00115409fb42081f2ca1
SachinGudMed <sachin.pawar@gudmed.in>
2026-07-03 12:12:00 +0530

    fix

--- src/components/ScribbleSection.jsx
+++ src/components/ScribbleSection.jsx
@@ -6,7 +6,7 @@ const ScribbleSection = () => {
           {/* Left - Image */}
           <div className="flex justify-center">
             <img
-              src="public/Scribble.png"
+              src="/Scribble.png"
               alt="GudMed Prescription Digitization"
               className="w-full max-w-2xl h-auto rounded-2xl shadow-xl"
             />
